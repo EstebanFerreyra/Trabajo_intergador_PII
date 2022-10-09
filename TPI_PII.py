d8a9e8b7b865849687f331e809d7bee02851be5e
EstebanFerreyra <eferreyra098@gmail.com>
2022-10-09 16:23:11 -0300

    Cambios en funcion 6, 9 y 10

--- TPI_PII.py
+++ TPI_PII.py
@@ -2,10 +2,11 @@ import sqlite3
 
 class ProgramaPrincipal:
 
-    def menu(self):
+    def menu(self) -> None:
         opcion: int = 99
         while opcion != 0:
-            print("MENU DE OPCIONES")
+            print("\n--------------------------------------------------------------------------")
+            print("\tMENU DE OPCIONES")
             print("1 - Cargar monopatienes")
             print("2 - Modificar datos de un monopatin")
             print("3 - Borrar un monopatin")
@@ -14,65 +15,92 @@ class ProgramaPrincipal:
             print("6 - Tabla")
             print("7 - Tabla de historico/precios")
             print("8 - Registros anteriores a una fecha en específico de la tabla monopatin")
-            print("9 - PRIMERA Y UNICA VEZ")
+            print("9 - CREAR TABLA MONOPATINES")
+            print("10 - BORRAR TABLA MONOPATINES")
             print("0 - Salir")
             opcion = int(input("Ingrese el numero correspondiente a la seccion: "))
-            if opcion >=0 and opcion <= 9:
+            print("--------------------------------------------------------------------------")
+            if opcion >=0 and opcion <= 10:
                 if opcion == 0:
                     break
                 elif opcion == 1:
-                    print("CARGA DE MONOPATINES")
+                    print("\n\tCARGA DE MONOPATINES")
                     marca = str(input("Ingrese la marca del monopatin: "))
                     precio = float(input("Ingrese el precio del monopatin: "))
                     stock = int(input("Ingrese la cantidad disponible: "))
                     nuevo_monopatin = Monopatin(marca, precio, stock)
                     nuevo_monopatin.cargar_monopatin()
                 elif opcion == 2:
-                    print("MODIFCAR EL PRECIO DE UN MONOPATIN")
+                    print("\n\tMODIFCAR EL PRECIO DE UN MONOPATIN")
                     nuevo_id = int(input("Ingrese ID del monopatin que desea modificar: "))
                     nuevo_precio = float(input("Ingrese el nuevo precio del monopatin: "))
                     Monopatin.modificar_precio(self, nuevo_id, nuevo_precio)
                 elif opcion == 3:
-                    print("ELIMINAR MONOPATIN")
+                    print("\n\tELIMINAR MONOPATIN")
                     borrar_id = int(input("Ingrese ID del monopatin que desea eliminar: "))
                     Monopatin.eliminar_monopatin(self, borrar_id) 
                 elif opcion == 4:
-                    print("MODIFICAR DISPONIBILIDAD")
+                    # ! REVISAR ENUNCIADO, PIDE QUE AUMENTE DE A UNO ME PARECE
+                    print("\n\tMODIFICAR DISPONIBILIDAD")
                     nuevo_id = int(input("Ingrese ID del monopatin que desea modificar la disponibilidad: "))
                     nuevo_stock = int(input("Ingrese la cantidad que desee agregar al stock: ")) 
                     Monopatin.cargar_disponibilidad(self, nuevo_id, nuevo_stock)
                 elif opcion == 5:
-                    print("LISTADO DE MONOPATINES")
+                    print("\n\tLISTADO DE MONOPATINES")
                     Monopatin.obtener_monopatines(self, "SELECT * FROM BdMonopatines")
                 elif opcion == 6:
-                    tabla()
+                    # ! CREAR TABLA CORRECTAMENTE COMO DICE ENUNCIADO
+                    borrar_sql = "DROP TABLE IF EXISTS Monopatin"
+                    sql = "CREATE TABLE Monopatin (_id INTEGER PRIMARY KEY , marca  VARCHAR(30), precio FLOAT NOT NULL, stock INTEGER NOT NULL,UNIQUE(marca))"
+                    self.crearTablas(borrar_sql, sql)
                 elif opcion == 7:
                     tabla_historico_precios()
                 elif opcion == 8:
                     registros_anteriores()
                 elif opcion == 9:
-                    self.crearTablas()
+                    borrar_sql = "DROP TABLE IF EXISTS BdMonopatines"
+                    sql = "CREATE TABLE BdMonopatines (_id INTEGER PRIMARY KEY , marca  VARCHAR(30), precio FLOAT NOT NULL, stock INTEGER NOT NULL,UNIQUE(marca))"
+                    self.crearTablas(borrar_sql, sql)
+                elif opcion == 10:
+                    self.borrar_tablas()
                 else:
                     print("Opcion invalida - Reingresar")
             else:
                 print("Opcion incorrecta - Reingresar")
 
-    def crearTablas(self):
-        conexion = Conexiones() #Esto
-        conexion.abrirConexion() #ESTO
-        conexion.miCursor.execute("DROP TABLE IF EXISTS BdMonopatines")
-        conexion.miCursor.execute("CREATE TABLE BdMonopatines (_id INTEGER PRIMARY KEY , marca  VARCHAR(30), precio FLOAT NOT NULL, stock INTEGER NOT NULL,UNIQUE(marca))")    
-        conexion.miConexion.commit()  #ESTO     
-        conexion.cerrarConexion() # ESTO SIEMPRE LO MISMO
+    def crearTablas(self, borrar_sql, sql) -> None:
+        conexion = Conexiones() 
+        conexion.abrirConexion() 
+        try:
+            conexion.miCursor.execute(borrar_sql)
+            conexion.miCursor.execute(sql)    
+            conexion.miConexion.commit()
+            print("Tabla creada correctamente")
+        except:
+            print("Error al crear tabla")
+        finally:             
+            conexion.cerrarConexion() 
+
+    def borrar_tablas(self) -> None:
+        conexion = Conexiones() 
+        conexion.abrirConexion()
+        try:
+            conexion.miCursor.execute("DROP TABLE IF EXISTS BdMonopatines")
+            conexion.miConexion.commit()   
+            print("Tabla monopatines eliminada correctamente")
+        except:
+            print("Error al eliminar tabla monopatines") 
+        finally: 
+            conexion.cerrarConexion() 
 
 
 class Conexiones:
 
-    def abrirConexion(self):
+    def abrirConexion(self) -> None:
         self.miConexion = sqlite3.connect("BdMonopatines")
         self.miCursor = self.miConexion.cursor()
         
-    def cerrarConexion(self):
+    def cerrarConexion(self) -> None:
         self.miConexion.close() 
 
 
@@ -145,18 +173,13 @@ class Monopatin():
         try:
             conexion.miCursor.execute(sql)
             monopatines = conexion.miCursor.fetchall()
-            print("Correcta visualizacion de lista")
+            for m in monopatines:
+                print(m)
         except:
             print("Error al mostrar la lista de monopatines")
         finally:
             conexion.cerrarConexion()
 
-        for m in monopatines:
-            print(m)
-
-
-def tabla():
-    print("opcio 6")
 
 def tabla_historico_precios():
     print("opcion 7")
@@ -166,5 +189,12 @@ def registros_anteriores():
 
 
 # CREACION DEL OBJETO PROGRAMA
-programa = ProgramaPrincipal()
-programa.menu()
+try:
+    programa = ProgramaPrincipal()
+    programa.menu()
+except:
+    print("\n\tERROR DE PROGRAMA")
+    print("ATENCION")
+    print(" - Controlar que la tabla monopatines se encuentre correctamente creada")
+finally:
+    print("Fin del programa")
